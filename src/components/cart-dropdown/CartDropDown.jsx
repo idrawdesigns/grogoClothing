56c591a10a0ab3a1f4973cd5ed6bd14417997000
idrawdesignsa <don@kesholabs.com>
2020-04-02 21:50:33 +0300

    Added cart-item component and connected our react dropdown with creatitem reducer

--- src/components/cart-dropdown/CartDropDown.jsx
+++ src/components/cart-dropdown/CartDropDown.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button/CustomButton'
 
 import './cart-dropdown.styles.scss'
+import CartItem from '../cart-item/CartItem'
 
-const CartDropDown = () => {
+const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
 
-export default CartDropDown
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+
+export default connect(mapStateToProps, null)(CartDropDown)
